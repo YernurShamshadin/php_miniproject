356935573a7b5314d8ce4a5cd49fd2e903aa248e
Yernur Shamshadin <ernurshamshadin@gmail.com>
2023-01-13 00:50:49 +0600

    Инициализация git-repo

---
+++ src/Http/Controllers/AdvertController.php
@@ -0,0 +1,50 @@
+<?php 
+
+namespace Shamshadinye\MyPhpProject\Http\Controllers; 
+
+use Shamshadinye\MyPhpProject\Model\Repository\AdvertRepository;
+use Shamshadinye\MyPhpProject\Model\Validators\AdvertValidator;
+use Slim\Http\ServerRequest;
+use Slim\Http\Response;
+use Slim\Views\Twig;
+
+class AdvertController
+{
+	public function index(ServerRequest $request, Response $response)
+	{
+		$advertsRepo = new AdvertRepository();
+		$adverts	 = $advertsRepo->getAll();
+		
+		$view = Twig::fromRequest($request);
+		
+		return $view->render($response, 'adverts/index.twig', ['adverts' => $adverts]);
+	}
+	
+	public function newAdvert(ServerRequest $request, Response $response) {
+        $view = Twig::fromRequest($request);
+
+        return $view->render($response, 'adverts/new.twig');
+    }
+
+    public function create(ServerRequest $request, Response $response)
+    {
+        $repo        = new AdvertRepository();
+        $advertData  = $request->getParsedBodyParam('advert', []);
+        
+        $validator = new AdvertValidator();
+        $errors    = $validator->validate($advertData);
+
+        if (!empty($errors)) {
+            $view = Twig::fromRequest($request);
+
+            return $view->render($response, 'adverts/new.twig', [
+                'data'   => $advertData,
+                'errors' => $errors,
+            ]);
+        }
+
+        $repo->create($advertData);
+
+        return $response->withRedirect('/adverts');
+    }
+}
---
+++ src/Model/Entity/Advert.php
@@ -0,0 +1,39 @@
+<?php 
+
+namespace Shamshadinye\MyPhpProject\Model\Entity;
+
+class Advert
+{
+	private ?int	$id;
+	private ?string $title;
+	private ?string $description;
+	private ?int	$price;
+	
+	public function __construct($data = [])
+	{
+		$this->id = $data['id'] ?? null;
+		$this->title = $data['title'] ?? null;
+		$this->description = $data['description'] ?? null;
+		$this->price = $data['price'] ?? null;
+	}
+	
+	public function getId(): ?int
+	{
+		return $this->id;
+	}
+	
+	public function getTitle(): string
+	{
+		return $this->title ?? '';
+	}
+	
+	public function getDescription(): string
+	{
+		return $this->description ?? '';
+	}
+	
+	public function getPrice(): ?int
+	{
+		return $this->price;
+	}
+}
